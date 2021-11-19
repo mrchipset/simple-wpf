29033aa9e3fb37332e1472e5b222fcee6d1cd4b6
X.F.Zhou <mrchip.zxf@outlook.com>
2021-11-19 14:45:58 +0800

    FEAT: promote style template binding
    Reference:
    https://stackoverflow.com/questions/6346594/templatebinding-does-not-work-in-certain-caseswhen-using-translatetransform

---
+++ Buttons/StyledLongPressButton/ArrowButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using LongPressButton;
+
+namespace StyledLongPressButton
+{
+    public class ArrowButton : LongPressButtonEx
+    {
+        public static readonly DependencyProperty AngleProperty
+            = DependencyProperty.Register("Angle",
+                typeof(double),
+                typeof(ArrowButton),
+                new PropertyMetadata(0.0));
+
+        public double Angle
+        {
+            set => SetValue(AngleProperty, value);
+            get => (double)GetValue(AngleProperty);
+        }
+
+    }
+}
