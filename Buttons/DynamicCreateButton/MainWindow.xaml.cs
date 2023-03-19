9d9b6c6a402286cf6138792ad028674aa48723be
X.F.Zhou <mrchip.zxf@outlook.com>
2023-03-19 21:50:24 +0800

    FEAT: add a dynamic button

---
+++ Buttons/DynamicCreateButton/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DynamicCreateButton
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public Button button;
+		public Property property;
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			button = new Button();
+			button.Width = 256;
+			button.Height = 24;
+			button.Name = "Button";
+			property= new Property();
+			Binding newBinding= new Binding("Name");
+			newBinding.Source = property;
+			button.SetBinding(Button.ContentProperty, newBinding);
+			button.Click += Button_Click;
+			this.container.Children.Add(button);
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			property.Name = "Clicked";
+		}
+	}
+}
---
+++ Buttons/DynamicCreateButton/Property.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicCreateButton
+{
+	public class Property : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				if (_name != value)
+				{
+					_name = value;
+					ONPropertyChanged();
+				}
+			}
+		}
+
+		public Property()
+		{
+			Name = "Button";
+		}
+
+		protected void ONPropertyChanged([CallerMemberName] string propertyName=null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
