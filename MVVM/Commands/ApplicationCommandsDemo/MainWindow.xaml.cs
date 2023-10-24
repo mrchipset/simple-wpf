53ca68b324bd372a76594e507a96a078a26d4e20
Mr.Chip <mrchip.zxf@outlook.com>
2023-10-24 17:14:28 +0800

    FEAT: add an application preset command new demo

---
+++ MVVM/Commands/ApplicationCommandsDemo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationCommandsDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show($"New command is executed by {e.Source.ToString()}");
+            e.Handled = true;
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            //if (e.Source is Button)
+            //{
+            //    e.CanExecute = true;
+            //} else
+            //{
+            //    e.CanExecute = false;
+            //}
+            e.CanExecute = true;
+        }
+    }
+}
