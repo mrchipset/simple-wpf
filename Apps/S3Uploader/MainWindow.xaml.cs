d5970b38a096853e249253157471d8ea5ef9c624
Mr.Chip <mrchip.zxf@outlook.com>
2023-12-12 14:51:46 +0800

    FEAT: add multi-part upload function to S3Uploader

--- Apps/S3Uploader/MainWindow.xaml.cs
+++ Apps/S3Uploader/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Amazon.S3.Transfer;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -32,7 +33,8 @@ namespace S3Uploader
 
         private string _bucket;
         private string _uploadFile;
-
+        private string _largeBucket;
+        private string _uploadLargeFile;
 
         private string _endpoint;
         private string _accessKey;
@@ -60,6 +62,32 @@ namespace S3Uploader
                 {
                     _uploadFile = value;
                     notifyPropertyChanged(nameof(UploadFile));
+                }
+            }
+        }
+
+        public string LargeBucket
+        {
+            get => _largeBucket;
+            set
+            {
+                if (_largeBucket != value)
+                {
+                    _largeBucket = value;
+                    notifyPropertyChanged(nameof(LargeBucket));
+                }
+            }
+        }
+
+        public string UploadLargeFile
+        {
+            get => _uploadLargeFile;
+            set
+            {
+                if (_uploadLargeFile != value)
+                {
+                    _uploadLargeFile = value;
+                    notifyPropertyChanged(nameof(UploadLargeFile));
                 }
             }
         }
@@ -172,6 +200,93 @@ namespace S3Uploader
                 }   
             }
             return ret;
+        }
+
+        private async Task<bool> UploadLargeFileAsync()
+        {
+            var credentials = new BasicAWSCredentials(_accessKey, _secretKey);
+
+            var clientConfig = new AmazonS3Config
+            {
+                ForcePathStyle = true,
+                ServiceURL = _endpoint,
+            };
+
+            bool ret = true;
+            using (var client = new AmazonS3Client(credentials, clientConfig))
+            {
+
+
+                try
+                {
+                    var fileTransferUtility = new TransferUtility(client);
+
+                    var uploadRequest = new TransferUtilityUploadRequest
+                    {
+                        BucketName = LargeBucket,
+                        FilePath = UploadLargeFile,
+                        Key = "key"
+                    };
+
+                    uploadRequest.UploadProgressEvent += UploadRequest_UploadProgressEvent;
+
+                    await fileTransferUtility.UploadAsync(uploadRequest);
+                }
+                catch (FileNotFoundException e)
+                {
+                    ret = false;
+                    this.Dispatcher.Invoke(new Action(() => this.statusLargeTxtBlk.Text = e.Message));
+                }
+                catch (AmazonS3Exception e)
+                {
+                    ret = false;
+                    if (e.ErrorCode != null &&
+                        (e.ErrorCode.Equals("InvalidAccessKeyId") ||
+                    e.ErrorCode.Equals("InvalidSecurity")))
+                    {
+                        this.Dispatcher.Invoke(new Action(() => this.statusLargeTxtBlk.Text = "Please check the provided AWS Credentials"));
+                    }
+                    else
+                    {
+                        this.Dispatcher.Invoke(new Action(() => this.statusLargeTxtBlk.Text = $"An error occurred with the message '{e.Message}' when writing an object"));
+                    }
+                }
+                catch(Exception e)
+                {
+                    this.Dispatcher.Invoke(new Action(() => this.statusLargeTxtBlk.Text = $"An error occurred with the message '{e.Message}' when writing an object"));
+                }
+            }
+            return ret;
+        }
+
+        private void UploadRequest_UploadProgressEvent(object? sender, UploadProgressArgs e)
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                this.uploadProgress.Value = e.TransferredBytes * 100 / e.TotalBytes ;
+            }));
+        }
+
+        private async void uploadLargeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Bucket: {LargeBucket}");
+            sb.AppendLine($"File: {UploadLargeFile}");
+            statusLargeTxtBlk.Text = sb.ToString();
+            var ret = await UploadLargeFileAsync();
+            if (ret)
+            {
+                statusLargeTxtBlk.Text = "Upload Successfully!";
+            }
+        }
+
+        private void browseLargeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                UploadLargeFile = openFileDialog.FileName;
+            }
         }
     }
 }
