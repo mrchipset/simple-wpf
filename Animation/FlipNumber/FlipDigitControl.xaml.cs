3784405f5af0b0a1e88df4739f8bcd6ecf39b8dc
Mr.Chip <mrchip.zxf@outlook.com>
2024-04-09 17:40:23 +0800

    FEAT: promote flip digital number

---
+++ Animation/FlipNumber/FlipDigitControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlipNumber
+{
+    /// <summary>
+    /// FlipDigitControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class FlipDigitControl : UserControl
+    {
+
+        public int CurrentNum {  get; set; }
+        public int PrevNum { get; set; }
+        public FlipDigitControl()
+        {
+            CurrentNum = 1;
+            PrevNum = 1;
+            this.DataContext = this;
+            InitializeComponent();
+
+            System.Diagnostics.Trace.WriteLine("");
+        }
+    }
+}
---
+++ Animation/FlipNumber/NumberDigitControl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FlipNumber
+{
+    public class NumberDigitControl : Control
+    {
+        public static readonly DependencyProperty NumberProperty = DependencyProperty.Register("Number", typeof(int), typeof(NumberDigitControl));   
+        public int Number
+        {
+            get
+            {
+                int? v = GetValue(NumberProperty) as int?;
+                return v ?? 0;
+            }
+            set
+            {
+                SetValue(NumberProperty, value);
+            }
+        }
+        public NumberDigitControl() : base()
+        {
+            MinWidth = 32;
+            MinHeight = 64;
+            FontSize = 32;
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            return base.ArrangeOverride(arrangeBounds);
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return base.MeasureOverride(constraint);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            double dip = VisualTreeHelper.GetDpi(this).PixelsPerDip;
+            FormattedText formattedText = new FormattedText(Number.ToString(),
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(FontFamily, FontStyle, FontWeight, FontStretch),
+                FontSize,
+                this.Foreground ?? Brushes.Black,
+                VisualTreeHelper.GetDpi(this).PixelsPerDip
+                );
+            formattedText.TextAlignment = TextAlignment.Center;
+            
+            double width = formattedText.Width;
+            double height = formattedText.Height;
+            double centerX = this.ActualWidth / 2;
+            double centerY = this.ActualHeight / 2 - height / 2;
+            //drawingContext.DrawRectangle(Background ?? Brushes.Black, new Pen(BorderBrush ?? Brushes.White, 1), new Rect(0, 0, this.ActualWidth, this.ActualHeight));
+            drawingContext.DrawRoundedRectangle(Background ?? Brushes.Black, new Pen(BorderBrush ?? Brushes.White, 1), new Rect(15, 15, this.ActualWidth - 30, this.ActualHeight - 30), 15, 15);
+            drawingContext.DrawText(formattedText, new Point(centerX, centerY));
+        }
+    }
+}
