b189f9b40bfb090c4adb99b8428c87d27fe0b064
Mr.Chip <mrchip.zxf@outlook.com>
2024-05-21 12:59:06 +0800

    FEAT: add basic splash window project

---
+++ Windows/SplashWindow/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SplashWindow
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //MainWindow mainwindow = new MainWindow();
+            //this.MainWindow = mainwindow;
+            MySplashWindow splash = new MySplashWindow();
+            splash.Show();
+            base.OnStartup(e);
+            //mainwindow.Show();
+            //splash.Close();
+            
+
+        }
+
+    }
+
+}
---
+++ Windows/SplashWindow/MySplashWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SplashWindow
+{
+    /// <summary>
+    /// SplashWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MySplashWindow : Window
+    {
+        public MySplashWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+
+            Dispatcher.BeginInvoke(DispatcherPriority.Loaded, (DispatcherOperationCallback)(arg =>
+            {
+                ((MySplashWindow)arg).Close();
+                return null;
+            }), this);
+        }
+    }
+}
