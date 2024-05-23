1e423225860b9883d3de4817c11d518ceed6e5c1
Mr.Chip <mrchip.zxf@outlook.com>
2024-05-23 15:00:08 +0800

    FEAT: add fadeout function

--- Windows/SplashWindow/MySplashWindow.xaml.cs
+++ Windows/SplashWindow/MySplashWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
@@ -20,6 +21,10 @@ namespace SplashWindow
     /// </summary>
     public partial class MySplashWindow : Window
     {
+        public int FadeoutDuration { get; set; } = 200;
+        private Storyboard storyboard = new Storyboard();
+        private DoubleAnimation fadeAnimation = new DoubleAnimation();
+
         public MySplashWindow()
         {
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
@@ -27,9 +32,39 @@ namespace SplashWindow
 
             Dispatcher.BeginInvoke(DispatcherPriority.Loaded, (DispatcherOperationCallback)(arg =>
             {
-                ((MySplashWindow)arg).Close();
+                ((MySplashWindow)arg).Fadeout(FadeoutDuration);
                 return null;
             }), this);
+
+            storyboard.Completed += FadeoutCompleted;
+
+        }
+
+        private void Fadeout(int fadeoutDuration)
+        {
+            Storyboard.SetTarget(fadeAnimation, this);
+            Storyboard.SetTargetProperty(fadeAnimation,new PropertyPath("Opacity"));
+            fadeAnimation.From = 1;
+            fadeAnimation.To = 0;
+            fadeAnimation.Duration = TimeSpan.FromMilliseconds(fadeoutDuration);
+            storyboard.Children.Add(fadeAnimation);
+            storyboard.Begin();
+
+        }
+
+        private void FadeoutCompleted(object? sender, EventArgs e)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                Close();
+            } else
+            {
+                Dispatcher.BeginInvoke(DispatcherPriority.Normal, (DispatcherOperationCallback)(arg =>
+                {
+                    ((MySplashWindow)arg).Close();
+                    return null;
+                }), this);
+            }
         }
     }
 }
